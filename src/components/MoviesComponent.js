32254f962b9970c64051fa729aa74142ef806183
Nathan <Nathan@Juriaan.enclave.com>
2018-01-19 17:27:35 +0700

    Nathan

--- src/components/MoviesComponent.js
+++ src/components/MoviesComponent.js
@@ -1,39 +1,67 @@
 import React, { Component } from 'react';
-import { Text, View, Image,FlatList,StyleSheet, TouchableOpacity, } from 'react-native';
+import { Text, View, Image, FlatList, StyleSheet, TouchableOpacity, } from 'react-native';
 import HeaderComponent from './HeaderComponent'
-import {api_now_playing} from '../../api'
-import { error } from 'util';
+import { api_now_playing } from '../../api'
+
+
 export default class MoviesComponent extends Component {
-    constructor(props){
+    constructor(props) {
+        super(props);
         this.state = {
             moviesPlaying: [],
         }
     }
-    componentDidMount(){
-        this.refreshData();
-    }
-    getMoviesNowPlaying=()=>{
-        await fetch(api_now_playing)
+    // componentWillMount() {
+    //     fetch(api_now_playing)
+    //         .then((response) => response.json())
+    //         .then((responseJson) => {
+    //             //return responseJson.movies;
+    //             console.log(responseJson)
+    //             //   return responseJson.results 
+    //             this.setState(() => {
+    //                 moviesPlaying: responseJson.results
+    //             })
+    //         })
+    //         .catch((error) => {
+    //             console.log(error);
+    //         });
+    // }
+    componentDidMount() {
+       return(
+        fetch(api_now_playing)
         .then((response) => response.json())
         .then((responseJson) => {
-          //return responseJson.movies;
-          console.log(responseJson.results)
-          return responseJson.results })
-        
+            this.setState( {moviesPlaying: responseJson.results})
+            console.log(this.state)
+        })
         .catch((error) => {
-          console.error(error);
-        });
+            console.log(error);
+        })
+       )
     }
-    refreshData=()=>{
-        getMoviesNowPlaying().then((movies)=>{
-            moviesPlaying:movies
-        }).catch((error));
-    }
+    // getMoviesNowPlaying = () => {
+    //     console.log('HI Nathan')
+    //     fetch(api_now_playing)
+    //         .then((response) => response.json())
+    //         .then((responseJson) => {
+    //             //return responseJson.movies;
+    //             console.log(responseJson)
+    //             //   return responseJson.results 
+    //         })
+    //         .catch((error) => {
+    //             console.log(error);
+    //         });
+    // }
+    // refreshData = () => {
+    //     this.getMoviesNowPlaying.then((movies) => {
+    //         moviesPlaying: movies
+    //     }).catch((error));
+    // }
     static navigationOptions = ({ navigation }) => {
-        
+
         let tabBarLabel = 'Movies';
-        let drawerLabel= 'Notifications';
-        
+        let drawerLabel = 'Notifications';
+
         let tabBarIcon = () => (
             <Image
                 source={require('../icons/home.png')}
@@ -44,71 +72,65 @@ export default class MoviesComponent extends Component {
 
     }
     render() {
-        
-        return (
-            <View style={{flex: 1, marginTop: 22}}>
-            <FlatList 
-                data = {this.state.moviesPlaying}
-                renderItem={({item, index})=>{
-                    //console.log(`Item = ${JSON.stringify(item)}, index = ${index}`);
-                    return (
-                    <FlatListItem item={item} index={index} parentFlatlis={this}>
 
-                    </FlatListItem>);
-                }}
-                keyExtractor={(item, index) => item.id}
+        return (
+            <View style={{ flex: 1, marginTop: 22 }}>
+                <FlatList
+                    data={this.state.moviesPlaying}
+                    renderItem={( {item, index} ) => <Text>{item.title}</Text>}
+                    keyExtractor={(item, index) => item.id}
                 >
 
-            </FlatList>
-            
-            
-        </View>
+                </FlatList>
+
+
+            </View>
         );
     }
 }
 
 class FlatListItem extends Component {
-    render() {          
-        return (        
+    render() {
+        return (
             // <View style={{
             //     flex: 1,
             //     flexDirection:'column',                                
             // }}>            
-                // {/* <View style={{
-                //         flex: 1,
-                //         flexDirection:'row',
-                //         // backgroundColor: this.props.index % 2 == 0 ? 'mediumseagreen': 'tomato'                
-                //         backgroundColor: 'mediumseagreen'
-                // }}>             */}
-                //     {/* <Image 
-                //         source={{uri: this.props.item.imageUrl}}
-                //         style={{width: 100, height: 100, margin: 5}}
-                //     >
+            // {/* <View style={{
+            //         flex: 1,
+            //         flexDirection:'row',
+            //         // backgroundColor: this.props.index % 2 == 0 ? 'mediumseagreen': 'tomato'                
+            //         backgroundColor: 'mediumseagreen'
+            // }}>             */}
+            //     {/* <Image 
+            //         source={{uri: this.props.item.imageUrl}}
+            //         style={{width: 100, height: 100, margin: 5}}
+            //     >
 
-                //     </Image> */}
-                    <View style={{
-                            flex: 1,
-                            flexDirection:'column',   
-                            height: 100                 
-                        }}>            
-                            <Text style={styles.flatListItem}>{this.props.item.title}</Text>
-                            <Text style={styles.flatListItem}>{this.props.item.overview}</Text>
-                    </View>              
-                // {/* </View> */}
-                // {/* <View style={{
-                //     height: 1,
-                //     backgroundColor:'white'                            
-                // }}>
-            
-                // </View> */}
-        //   </View>
-    );
+            //     </Image> */}
+            <View style={{
+                flex: 1,
+                flexDirection: 'column',
+                height: 100
+            }}>
+                <Text style={styles.flatListItem}>{this.props.item.title}</Text>
+                <Text style={styles.flatListItem}>{this.props.item.overview}</Text>
+            </View>
+            // {/* </View> */}
+            // {/* <View style={{
+            //     height: 1,
+            //     backgroundColor:'white'                            
+            // }}>
+
+            // </View> */}
+            //   </View>
+        );
     }
 }
 const styles = StyleSheet.create({
     flatListItem: {
         color: 'white',
         padding: 10,
-        fontSize: 16,  
+        fontSize: 16,
     }
 });
